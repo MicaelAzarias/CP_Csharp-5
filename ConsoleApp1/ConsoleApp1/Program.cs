0182bea231d03ad5067a0cc845cc4b23bf824350
MicaelAzarias <143201335+MicaelAzarias@users.noreply.github.com>
2025-10-06 11:17:59 -0300

    Exercicio 5 do CP
    Criar a classe de livros e seus metodos

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public class Livro
+        {
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public int AnoPublicacao { get; set; }
+
+            public Livro(string titulo, string autor, int anoPublicacao)
+            {
+                Titulo = titulo;
+                Autor = autor;
+                AnoPublicacao = anoPublicacao;
+            }
+
+
+            public void ExibirDetalhes()
+            {
+                Console.WriteLine($"Título: {Titulo}, Autor: {Autor}, Ano de Publicação: {AnoPublicacao}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro("Diario de um bana", "Jeff Kinney", 2007);
+            livro.ExibirDetalhes();
+        }
+    }
+}
